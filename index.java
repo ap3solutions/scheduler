58d3841f44a029338de4c7775c82ffd99c2e42c5
ap3solutions <122047203+ap3solutions@users.noreply.github.com>
2023-01-05 14:34:10 -0400

    Delete index.java

--- index.java
+++
@@ -1,8 +0,0 @@
-<form id="availability-form">
-  <label for="monday">Monday</label>
-  <input type="checkbox" id="monday" name="monday" value="Monday">
-  <label for="tuesday">Tuesday</label>
-  <input type="checkbox" id="tuesday" name="tuesday" value="Tuesday">
-  <!-- other days of the week -->
-  <button type="submit">Submit</button>
-</form>
