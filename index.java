2d614c94f25e373f11f1489cfc573d63a2a8a376
ap3solutions <122047203+ap3solutions@users.noreply.github.com>
2023-01-05 14:31:36 -0400

    Create index.java

---
+++ index.java
@@ -0,0 +1,8 @@
+<form id="availability-form">
+  <label for="monday">Monday</label>
+  <input type="checkbox" id="monday" name="monday" value="Monday">
+  <label for="tuesday">Tuesday</label>
+  <input type="checkbox" id="tuesday" name="tuesday" value="Tuesday">
+  <!-- other days of the week -->
+  <button type="submit">Submit</button>
+</form>
